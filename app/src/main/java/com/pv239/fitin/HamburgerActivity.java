723a0ae3c8ebc5dd1b53f100174d85dceccbf568
Ondrej Pontes <opontes@op.brq.redhat.com>
2016-04-14 11:09:45 +0200

    Fix bugs for AS 2.1

---
+++ app/src/main/java/com/pv239/fitin/HamburgerActivity.java
@@ -0,0 +1,9 @@
+package com.pv239.fitin;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by opontes on 12/04/16.
+ */
+public class HamburgerActivity extends AppCompatActivity {
+}
