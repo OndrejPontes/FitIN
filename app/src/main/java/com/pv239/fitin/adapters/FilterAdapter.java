c9d6a0b7a7f800bef6b04e2d529e60ac2a4d6278
xnieslan <nieslanik.radovan@gmail.com>
2016-05-31 11:45:53 +0200

    Generated Data to https://grepgym.firebaseio.com/ except Users - to discuss

--- app/src/main/java/com/pv239/fitin/adapters/FilterAdapter.java
+++ app/src/main/java/com/pv239/fitin/adapters/FilterAdapter.java
@@ -1,50 +1,130 @@
 package com.pv239.fitin.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.pv239.fitin.Entities.Filter;
+import com.pv239.fitin.Entities.GymPreview;
 import com.pv239.fitin.R;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+import java.util.List;
 
 /**
  * Created by xnieslan on 30.05.2016.
  */
 public class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.FilterHolder> {
 
+//    @Override
+//    public FilterHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(FilterHolder holder, int position) {
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//
+//    class FilterHolder extends RecyclerView.ViewHolder{
+//        View container;
+//        TextView reviewText;
+//        CircleImageView userProfileImage;
+//        TextView ratingText;
+//        int height;
+//
+//        public FilterHolder(View itemView) {
+//            super(itemView);
+//            container = itemView.findViewById(R.id.review_container);
+//            reviewText= (TextView) itemView.findViewById(R.id.review_text);
+//            userProfileImage = (CircleImageView) itemView.findViewById(R.id.review_user_profile_image);
+//            ratingText = (TextView) itemView.findViewById(R.id.review_rating_text);
+//            height = reviewText.getHeight();
+//        }
+//
+//    }
+
+
+    private List<Filter> filterList;
+    private LayoutInflater inflater;
+    private Context context;
+
+    private ItemClickCallback itemClickCallback;
+
+    public interface ItemClickCallback {
+        void onItemClick(int p);
+    }
+
+    public void setItemClickCallback(final ItemClickCallback itemClickCallback) {
+        this.itemClickCallback = itemClickCallback;
+    }
+
+    public FilterAdapter (List<Filter> filterList, Context context) {
+        this.filterList = filterList;
+        this.inflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
     @Override
     public FilterHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.my_filters_item, parent, false);
+        return new FilterHolder(view);
     }
 
     @Override
     public void onBindViewHolder(FilterHolder holder, int position) {
+//        Filter item = filterList.get(position);
+//        holder.gymPreviewName.setText(item.getName());
+//        holder.gymPreviewAddress.setText(item.getAddress());
+//        holder.gymPreviewRating.setText(item.getRating());
+//        Picasso.with(context)
+//                .load(item.getPhotoPreviewUrl())
+//                .resize(300, 150)
+//                .centerCrop()
+//                .into(holder.gymPreviewImage);
 
+    }
+
+    public void setListData(List<GymPreview> listData) {
+        this.filterList.clear();
+        this.filterList.addAll(filterList);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return filterList.size();
     }
 
-    class FilterHolder extends RecyclerView.ViewHolder{
+    class FilterHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
         View container;
-        TextView reviewText;
-        CircleImageView userProfileImage;
-        TextView ratingText;
-        int height;
+        TextView filterFragmentName;
+        TextView filterFragmentEquipments;
+        TextView filterFragmentActivities;
+        TextView filterFragmentCount;
 
         public FilterHolder(View itemView) {
             super(itemView);
-            container = itemView.findViewById(R.id.review_container);
-            reviewText= (TextView) itemView.findViewById(R.id.review_text);
-            userProfileImage = (CircleImageView) itemView.findViewById(R.id.review_user_profile_image);
-            ratingText = (TextView) itemView.findViewById(R.id.review_rating_text);
-            height = reviewText.getHeight();
+            filterFragmentName = (TextView) itemView.findViewById(R.id.my_filters_item_name);
+            filterFragmentEquipments = (TextView) itemView.findViewById(R.id.my_filters_item_equipments);
+            filterFragmentActivities = (TextView) itemView.findViewById(R.id.my_filters_item_activities);
+            filterFragmentCount = (TextView) itemView.findViewById(R.id.my_filters_item_count);
+
+            container = itemView.findViewById(R.id.my_filters_item_container);
+            container.setOnClickListener(this);
         }
 
+        @Override
+        public void onClick(View v) {
+            itemClickCallback.onItemClick(getAdapterPosition());
+        }
     }
 
 }
--- app/src/main/java/com/pv239/fitin/fragments/filter/MyFiltersFragment.java
+++ app/src/main/java/com/pv239/fitin/fragments/filter/MyFiltersFragment.java
@@ -4,6 +4,8 @@ package com.pv239.fitin.fragments.filter;
 import android.support.v4.app.Fragment;
 //import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +15,13 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.pv239.fitin.Entities.Activity;
 import com.pv239.fitin.Entities.Equipment;
+import com.pv239.fitin.Entities.Filter;
+import com.pv239.fitin.Entities.Gym;
+import com.pv239.fitin.Entities.Review;
 import com.pv239.fitin.R;
+import com.pv239.fitin.adapters.FilterAdapter;
 import com.pv239.fitin.utils.Constants;
 
 import java.util.ArrayList;
@@ -25,6 +32,9 @@ import java.util.Map;
 public class MyFiltersFragment extends Fragment {
 
     private Firebase ref;
+    private RecyclerView recyclerView;
+    private FilterAdapter filterAdapter;
+    private View rootView;
 
     public void setRef(Firebase ref) {
         this.ref = ref;
@@ -32,13 +42,10 @@ public class MyFiltersFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_my_filters, container, false);
+        rootView = inflater.inflate(R.layout.fragment_my_filters, container, false);
 
-//        for(int i = 0; i < 10; i++) {
-//            Equipment equipment = new Equipment("name" + i, "desc" + i);
-//            Firebase newEquipment = this.ref.push();
-//            newEquipment.setValue(equipment);
-//        }
+
+        //generateData();
 
         final List<Equipment> eqList = new ArrayList<>();
 
@@ -61,6 +68,66 @@ public class MyFiltersFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    private void setData (List<Filter> filterList){
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.my_filters_recycle_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        filterAdapter = new FilterAdapter(filterList, getActivity());
+        recyclerView.setAdapter(filterAdapter);
+
+    }
+
+    private void generateData(){
+
+
+        List<Review> reviewList = new ArrayList<>();
+        List<String> activitiesId = new ArrayList<>();
+        List<String> photoUrls = new ArrayList<>();
+        List<Gym> gyms = new ArrayList<>();
+        List<String> equipmentsId = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            Equipment equipment = new Equipment("name" + i, "desc" + i);
+            Firebase newEquipment = this.ref.push();
+            newEquipment.setValue(equipment);
+            equipmentsId.add(newEquipment.getKey());
+        }
+
+        Firebase activitiesRef = new Firebase(Constants.FIREBASE_REF + "activities");
+
+        for(int i = 0; i < 10; i++) {
+            Activity activity = new Activity("name" + i, "desc" + i);
+            Firebase newActivity = activitiesRef.push();
+            newActivity.setValue(activity);
+            activitiesId.add(newActivity.getKey());
+        }
+
+        Firebase gymsRef = new Firebase(Constants.FIREBASE_REF + "gyms");
+
+
+        for(int i = 0; i < 10; i++) {
+            Review review = new Review("userId" +i, "title" +i, "reviewText" +i, i, "userPhotoUrl"+i);
+            reviewList.add(review);
+            photoUrls.add("photoUrl" + i);
+        }
+
+        for(int i = 0; i < 10; i++) {
+            Gym gym = new Gym("name" + i, "desc" + i, i, "someUrl" + i, "someAddress" +i ,photoUrls, reviewList );
+            Firebase newGym = gymsRef.push();
+            newGym.setValue(gym);
+            gyms.add(gym);
+        }
+
+
+        Firebase filtersRef = new Firebase(Constants.FIREBASE_REF + "filters");
+
+        for(int i = 0; i < 10; i++) {
+            Filter filter = new Filter("filter" +i, gyms.get(i).getName(), equipmentsId, activitiesId );
+            Firebase newFilter = filtersRef.push();
+            newFilter.setValue(filter);
+        }
     }
 
 }
