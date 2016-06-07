017bd0f147def1ee0fda7c27d2f1bd3c0d3bb131
Vladimír Jarabica <vladojarabica@gmail.com>
2016-06-07 19:50:54 +0200

    Notifications

--- app/src/main/java/com/pv239/fitin/MainActivity.java
+++ app/src/main/java/com/pv239/fitin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pv239.fitin;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
@@ -20,9 +21,12 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.pv239.fitin.Entities.Activity;
 import com.pv239.fitin.Entities.Filter;
+import com.pv239.fitin.fragments.FragmentHelper;
 import com.pv239.fitin.fragments.filter.FilterFragment;
 import com.pv239.fitin.fragments.filter.MyFiltersFragment;
+import com.pv239.fitin.fragments.gym.GymFragment;
 import com.pv239.fitin.fragments.login.RegisterFragment;
 import com.pv239.fitin.fragments.results.ResultsFragment;
 import com.pv239.fitin.utils.DataManager;
@@ -30,6 +34,7 @@ import com.pv239.fitin.Entities.User;
 import com.pv239.fitin.fragments.home.HomeFragment;
 import com.pv239.fitin.fragments.login.LoginFragment;
 import com.pv239.fitin.utils.Constants;
+import com.pv239.fitin.utils.services.GymNotificationListener;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -52,7 +57,11 @@ public class MainActivity extends AppCompatActivity implements /*LoginFragment.O
         setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
 
+        final Intent intent = getIntent();
+
         this.ref = new Firebase(Constants.FIREBASE_REF);
+
+        final MainActivity self = this;
 
         loginFragment = new LoginFragment();
         loginFragment.setRef(ref);
@@ -84,7 +93,22 @@ public class MainActivity extends AppCompatActivity implements /*LoginFragment.O
                             u.setName(dataSnapshot.child("name").getValue().toString());
                             u.setProfileImageUrl(authData.getProviderData().get("profileImageURL").toString());
                             DataManager.getInstance().putObject(Constants.USER, u);
+                            startService(new Intent(self, GymNotificationListener.class));
                             updateUser();
+
+
+                            if(intent.getExtras() != null) {
+                                Object gymIdObject = intent.getExtras().get(Constants.GYM_ID);
+                                if (gymIdObject != null) {
+                                    String gymId = (String) gymIdObject;
+
+                                    GymFragment fragment = new GymFragment();
+                                    fragment.setId(gymId);
+                                    fragment.setRef(ref.child("gyms").child(gymId));
+
+                                    FragmentHelper.updateDisplay(getSupportFragmentManager(), fragment);
+                                }
+                            }
                         }
 
                         @Override
