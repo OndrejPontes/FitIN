5666a511021583c3c1a27c2ce78b1ea7f4fec458
Ondrej Pontes <opontes@op.brq.redhat.com>
2016-06-08 17:37:25 +0200

    Fix favourite gyms

--- app/src/main/java/com/pv239/fitin/utils/GymFiltering.java
+++ app/src/main/java/com/pv239/fitin/utils/GymFiltering.java
@@ -17,7 +17,7 @@ public class GymFiltering {
     private static Filter filter;
 
     public static List<GymPreview> filterGymsPreviews(Filter f, List<Gym> gyms, Integer best) {
-        for(Gym gym: gyms) {
+        for (Gym gym : gyms) {
             setGymRating(gym);
         }
         filteredGym = gyms;
@@ -25,72 +25,77 @@ public class GymFiltering {
 
         List<GymPreview> gymPreviews = new ArrayList<>();
 
-        if(filter != null && best == null){
-            if(filter.getGymName() != null) filterByGymName();
-            if(filter.getSouthWest() != null && filter.getNorthEast() != null) filterByLocation();
-            if(filter.getEquipments() != null && filter.getEquipments().size() != 0) filterByEquipments();
-            if(filter.getActivities() != null && filter.getActivities().size() != 0) filterByActivities();
-        } else {
+        if (filter != null) {
+            if (filter.getGymName() != null) filterByGymName();
+            if (filter.getSouthWest() != null && filter.getNorthEast() != null) filterByLocation();
+            if (filter.getEquipments() != null && filter.getEquipments().size() != 0)
+                filterByEquipments();
+            if (filter.getActivities() != null && filter.getActivities().size() != 0)
+                filterByActivities();
+        }
+        if (best != null) {
             Log.i(Constants.TAG, "Filter is null");
             Collections.sort(filteredGym);
             filteredGym = filteredGym.subList(0, best);
         }
 
-        for(Gym gym : filteredGym) {
+        for (Gym gym : filteredGym) {
             gymPreviews.add(gymToPreview(gym));
         }
         return gymPreviews;
     }
 
-    private static void filterByGymName(){
+    private static void filterByGymName() {
         String regex = filter.getGymName();
 
         List<Gym> help = new ArrayList<>(filteredGym);
         for (Gym gym : help) {
-            if(!gym.getName().contains(regex)) filteredGym.remove(gym);
+            if (!gym.getName().contains(regex)) filteredGym.remove(gym);
         }
     }
 
-    private static void filterByLocation(){
+    private static void filterByLocation() {
         Coordinates c1 = filter.getNorthEast();
         Coordinates c2 = filter.getSouthWest();
         List<Gym> help = new ArrayList<>(filteredGym);
         for (Gym gym : help) {
-            if(gym.getCoordinates().getLatitude() < c1.getLatitude() ||
+            if (gym.getCoordinates().getLatitude() < c1.getLatitude() ||
                     gym.getCoordinates().getLongitude() < c1.getLongitude() ||
                     gym.getCoordinates().getLatitude() > c2.getLatitude() ||
-                    gym.getCoordinates().getLongitude() > c2.getLongitude()) filteredGym.remove(gym);
+                    gym.getCoordinates().getLongitude() > c2.getLongitude())
+                filteredGym.remove(gym);
         }
     }
 
-    private static void filterByEquipments(){
+    private static void filterByEquipments() {
         List<String> equipments = filter.getEquipments();
         List<Gym> help = new ArrayList<>(filteredGym);
         boolean remove = true;
         if (equipments.size() == 0) return;
         for (Gym gym : help) {
             for (String equipment : equipments) {
-                if(gym.getEquipmentList().contains(equipment)) remove = false;
+                if (gym.getEquipmentList().contains(equipment)) remove = false;
             }
-            if(remove) filteredGym.remove(gym);
+            if (remove) filteredGym.remove(gym);
         }
     }
 
-    private static void filterByActivities(){
+    private static void filterByActivities() {
         List<String> activities = filter.getActivities();
         List<Gym> help = new ArrayList<>(filteredGym);
         if (activities.size() == 0) return;
         boolean remove = true;
         for (Gym gym : help) {
             for (String activity : activities) {
-                if(gym.getActivityList().contains(activity)) remove = false;
+                if (gym.getActivityList().contains(activity)) remove = false;
             }
-            if(remove) filteredGym.remove(gym);
+            if (remove) filteredGym.remove(gym);
         }
     }
 
     /**
      * Convert gym to gymPreview
+     *
      * @param gym - gym to convert
      * @return converted gymPreview
      */
@@ -99,15 +104,15 @@ public class GymFiltering {
         return gymPreview;
     }
 
-    public static void setGymRating (Gym gym){
+    public static void setGymRating(Gym gym) {
 
         List<Review> reviews = gym.getReviews();
 
         int totalRating = 0;
-        for(Review r : reviews){
+        for (Review r : reviews) {
             totalRating += r.getRating();
         }
 
-        gym.setRating( (int) Math.ceil(totalRating/reviews.size()));
+        gym.setRating((int) Math.ceil(totalRating / reviews.size()));
     }
 }
